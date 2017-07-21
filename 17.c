c0e98471b3d5db1cc87149569a0f2a843a5ad534
henryfung3a27 <henryfung3a27@yahoo.com.hk>
2017-07-21 18:06:09 +0800

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,55 @@
+/* 17. Letter Combinations of a Phone Number */
+
+/* The fun() is to merge the array of strings (a) and the char array (b).
+ * Then it returns the pointer to pointer to the array of new strings.
+ */
+
+
+char** fun(char** a, char b[], int *size) {
+    int len_b = strlen(b);
+    int len = len_b * (*size);      // size of new array of strings
+    int w = 0;
+    
+    char** res = malloc(len * sizeof(char*));
+    char temp[32] = "";
+    
+    for (int i = 0; i < len; i++) {
+        res[i] = (char*)calloc(strlen(a[0]) + 2, sizeof(char));
+    }
+    
+    for (int i = 0; i < len_b; i++) {
+        for (int k = 0; k < *size; k++) {
+            sprintf(temp, "%s%c", a[k], b[i]);
+            strcpy(res[w++], temp);
+        }
+    }
+    
+    *size = len;
+    return res;
+}
+
+char** letterCombinations(char* digits, int* returnSize) {
+    if (strlen(digits) < 1) return 0;
+    
+    char key[8][5] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    int len_digits = strlen(digits);
+    int temp;
+    
+    temp = digits[0] - '0' - 2;
+    *returnSize = strlen(key[temp]);
+    
+    char** ret = (char**)malloc((*returnSize) * sizeof(char*));
+    
+    for (int i = 0; i < *returnSize; i++) {
+        ret[i] = calloc(2, sizeof(char));
+        ret[i][0] = key[digits[0] - '0' - 2][i];
+    }
+    
+    for (int i = 1; i < len_digits; i++) {
+        ret = fun(ret, key[digits[i] - '0' - 2], returnSize);
+        // ret is keep on pointing to a new array of strings. Then, use the new
+        // pointer to merge new characters by giving the ret as a parameter of fun();
+    }
+    
+    return ret;
+}
